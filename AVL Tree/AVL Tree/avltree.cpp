50b61bfad7d09c85abb906c29f1a8a52d41620cf
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-11-17 14:26:24 +0900

    키 값의 유연성 제공을 위한 템플릿 적용

--- AVL Tree/AVL Tree/avltree.cpp
+++ AVL Tree/AVL Tree/avltree.cpp
@@ -3,31 +3,44 @@
 using namespace std;
 
 // 생성자
-AvlTree::AvlTree() { root_ = NULL; }
+template <typename T>
+AvlTree<T>::AvlTree() { root_ = NULL; }
 
 // 노드의 높이 구하는 함수
-int AvlTree::Height(Node *node) {
+template <typename T>
+int AvlTree<T>::Height(Node<T> *node)
+{
   if (node == NULL)
     return -1;
   return node->height;
 }
 
 // 노드의 높이를 업데이트하는 함수
-void AvlTree::UpdateHeight(Node *node) {
-  if (node != NULL)
+template <typename T>
+void AvlTree<T>::UpdateHeight(Node<T> *node)
+{
+  if (node != nullptr)
     node->height = 1 + max(Height(node->left_child), Height(node->right_child));
 }
 
 // 특정 키 값을 갖는 노드를 찾는 함수
-Node *AvlTree::FindNode(int data) {
-  Node *node = root_;
+template <typename T>
+Node<T> *AvlTree<T>::FindNode(T data)
+{
+  Node<T> *node = root_;
 
-  while (node != NULL) {
-    if (data == node->data) {
+  while (node != NULL)
+  {
+    if (data == node->data)
+    {
       return node;
-    } else if (data < node->data) {
+    }
+    else if (data < node->data)
+    {
       node = node->left_child;
-    } else {
+    }
+    else
+    {
       node = node->right_child;
     }
   }
@@ -36,7 +49,9 @@ Node *AvlTree::FindNode(int data) {
 }
 
 // 노드를 중심으로, Right roation을 수행하는 함수
-void AvlTree::RightRotate(Node *node) {
+template <typename T>
+void AvlTree<T>::RightRotate(Node<T> *node)
+{
   Node *left_child = node->left_child;
   node->left_child = left_child->right_child;
 
@@ -60,16 +75,18 @@ void AvlTree::RightRotate(Node *node) {
 }
 
 // 노드를 중심으로, Left roation을 수행하는 함수
-void AvlTree::LeftRotate(Node *node) {
-  Node *right_child = node->right_child;
+template <typename T>
+void AvlTree<T>::LeftRotate(Node<T> *node)
+{
+  Node<T> *right_child = node->right_child;
   node->right_child = right_child->left_child;
 
-  if (right_child->left_child != NULL)
+  if (right_child->left_child != nullptr)
     right_child->left_child->parent = node;
 
   right_child->parent = node->parent;
 
-  if (node->parent == NULL)
+  if (node->parent == nullptr)
     root_ = right_child;
   else if (node == node->parent->left_child)
     node->parent->left_child = right_child;
@@ -84,28 +101,34 @@ void AvlTree::LeftRotate(Node *node) {
 }
 
 // data 값을 갖는 노드가 root인 부분트리에서, 최소값 노드를 반환하는 함수
-Node *AvlTree::Minimum(int data) {
-  Node *node = FindNode(data);
+template <typename T>
+Node<T> *AvlTree<T>::Minimum(T data)
+{
+  Node<T> *node = FindNode(data);
 
-  if (node == NULL) {
-    return NULL;
+  if (node == nullptr)
+  {
+    return nullptr;
   }
 
-  while (node->left_child != NULL) {
+  while (node->left_child != nullptr)
+  {
     node = node->left_child;
   }
 
   return node;
 }
-
 // data 값을 갖는 노드가 root인 부분트리에서, 최댓값 노드를 반환하는 함수
-Node *AvlTree::Maximum(int data) {
-  Node *node = FindNode(data);
+template <typename T>
+Node<T> *AvlTree<T>::Maximum(T data)
+{
+  Node<T> *node = FindNode(data);
 
-  if (node == NULL)
-    return NULL;
+  if (node == nullptr)
+    return nullptr;
 
-  while (node->right_child != NULL) {
+  while (node->right_child != nullptr)
+  {
     node = node->right_child;
   }
 
@@ -113,37 +136,47 @@ Node *AvlTree::Maximum(int data) {
 }
 
 // 트리에 저장된 원소의 수를 반환
-int AvlTree::Size() {
+template <typename T>
+int AvlTree<T>::Size()
+{
   return size_;
 }
 
 // 트리가 비어있으면 1, 비어있지 않다면 0을 반환
-bool AvlTree::Empty() {
+template <typename T>
+bool AvlTree<T>::Empty()
+{
   return (size_ == 0);
-};
+}
 
-//data 값을 갖는 노드의 depth를 반환하고,
-//노드가 존재하지 않는다면 0을 반환하는 함수
-int AvlTree::Find(int data) {
-  if (root_ == NULL) return 0;
+// data 값을 갖는 노드의 depth를 반환하고,
+// 노드가 존재하지 않는다면 0을 반환하는 함수
+template <typename T>
+int AvlTree<T>::Find(T data)
+{
+  if (root_ == nullptr)
+    return 0;
 
-  Node *node = root_;
+  Node<T> *node = root_;
   int depth = 0;
 
-  while (node!=NULL) {
-    if (data == node->data) {
+  while (node != nullptr)
+  {
+    if (data == node->data)
+    {
       return depth;
     }
-    else if (data < node->data) {
+    else if (data < node->data)
+    {
       depth++;
       node = node->left_child;
     }
-    else {
+    else
+    {
       depth++;
       node = node->right_child;
     }
   }
 
   return 0;
-
 }
